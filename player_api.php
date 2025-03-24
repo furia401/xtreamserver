ffa72e3a66eb8e5b887ffd5328626d8edc04c34a
jose flavio <joseflavio401@gmail.com>
2025-03-24 19:32:57 -0300

    correção nas exibiçao das series no app  IPTV Stream Player

--- player_api.php
+++ player_api.php
@@ -1,34 +1,43 @@
 <?php
 function gerarCodigoUnico() {
-
+    // Pega o timestamp atual (número de segundos desde 1970)
     $timestamp = time();
 
+    // Extrai os últimos 4 dígitos do timestamp (pode ser ajustado dependendo da precisão)
     $codigo = substr($timestamp, -4);
 
+    // Caso o número extraído tenha menos de 4 dígitos, preenche com zeros à esquerda
     return str_pad($codigo, 4, '0', STR_PAD_LEFT);
 }
 
 function buscarSerieTMDB($query) {
     $apiKey = "30888e8f271f1698dcfb0228f589bec1";
 
+    // Remover "[L]" do nome antes da requisição
     $queryLimpa = str_replace("[L]", "", $query);
 
     $url = "http://api.themoviedb.org/3/search/tv?query=" . urlencode($query) . "&api_key=" . $apiKey . "&language=pt-BR";
 
+    // Inicializa cURL
     $ch = curl_init();
 
+    // Configurações do cURL
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Desativa verificação SSL (caso necessário)
 
+    // Executa a requisição
     $response = curl_exec($ch);
 
+    // Verifica erro na requisição
     if (curl_errno($ch)) {
         return json_encode(['erro' => 'Erro na requisição: ' . curl_error($ch)]);
     }
 
+    // Fecha conexão cURL
     curl_close($ch);
 
+    // Decodifica o JSON para um array associativo
     $dados = json_decode($response, true);
 
     return json_encode($dados, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
@@ -38,25 +47,33 @@ function buscarDetalhesSerieTMDB($tmdb_id) {
     $apiKey = "30888e8f271f1698dcfb0228f589bec1";
     $url = "https://api.themoviedb.org/3/tv/$tmdb_id?api_key=$apiKey&language=pt-BR&append_to_response=credits,videos";
 
+    // Inicializa cURL
     $ch = curl_init();
-
+    
+    // Configurações do cURL
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
+    // Executa a requisição
     $response = curl_exec($ch);
 
+    // Verifica erro na requisição
     if (curl_errno($ch)) {
         return json_encode(['erro' => 'Erro na requisição: ' . curl_error($ch)]);
     }
 
+    // Fecha conexão cURL
     curl_close($ch);
 
+    // Decodifica o JSON para um array associativo
     $dados = json_decode($response, true);
 
     return json_encode($dados, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 }
 
+
+// Conecta-se ao banco de dados de usuários
 require_once($_SERVER['DOCUMENT_ROOT'] . '/api/controles/db.php');
 header('Content-Type: application/json; charset=utf-8');
 header('Connection: close');
@@ -100,8 +117,9 @@ if(isset($_POST['username']) && isset($_POST['password'])) {
   exit;
 }
 
+//Autenticação /player_api.php?username=X&password=X
 if (!$username || !$password) {
-    http_response_code(401); 
+    http_response_code(401); // 401 Unauthorized
    $errorResponse['user_info'] = array();
    $errorResponse['user_info']['auth'] = 0;
    $errorResponse['user_info']['msg'] = "username e password necessario!";
@@ -119,12 +137,13 @@ $statement->bindValue(':password', $password);
 $statement->execute();
 $result = $statement->fetch(PDO::FETCH_ASSOC);
 if (!$result) {
-    http_response_code(401); 
+    http_response_code(401); // 401 Unauthorized
     $errorResponse = json_encode(["user_info" => ["auth" => 0]]);
     echo $errorResponse;
     exit();
 }
 
+//Autenticação /player_api.php?username=X&password=X
 if (isset($_GET['username']) && isset($_GET['password']) && !isset($_GET['action'])) {
 
     $exp_date = strtotime($result['Vencimento']);
@@ -165,16 +184,19 @@ if (isset($_GET['username']) && isset($_GET['password']) && !isset($_GET['action
             'timezone' => date_default_timezone_get()
         )
     );
-
+    // retorna a resposta em JSON
     echo json_encode($response);
     exit();
 }
 
+//-----GET Categorias de transmissão ao vivo
+//player_api.php?username=X&password=X&action=get_live_categories
 if (isset($_GET['action']) && in_array($_GET['action'], ['get_live_categories', 'get_vod_categories', 'get_series_categories'])) {
     $action = $_GET['action'];
     $adulto = $result['adulto'];
     $type = '';
 
+    // Define o tipo de categoria baseado na ação
     switch ($action) {
         case 'get_live_categories':
             $type = 'live';
@@ -187,6 +209,7 @@ if (isset($_GET['action']) && in_array($_GET['action'], ['get_live_categories', 
             break;
     }
 
+    // Prepara a query com ou sem filtro de adulto
     $query_str = "SELECT * FROM categoria WHERE type = :type";
     if ($adulto == 0) {
         $query_str .= " AND is_adult = '0'";
@@ -196,15 +219,17 @@ if (isset($_GET['action']) && in_array($_GET['action'], ['get_live_categories', 
     $query->bindValue(":type", $type);
     $query->execute();
 
+    // Processa os resultados
     $results = [];
     while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
         $results[] = [
-            "category_id" => (string)$row["id"], 
+            "category_id" => (string)$row["id"], // Cast para string
             "category_name" => $row["nome"],
             "parent_id" => $row["parent_id"],
         ];
     }
 
+    // Caso não haja resultados, adiciona uma categoria padrão
     if (empty($results)) {
         $results[] = [
             "category_id" => "1",
@@ -213,17 +238,22 @@ if (isset($_GET['action']) && in_array($_GET['action'], ['get_live_categories', 
         ];
     }
 
+    // Retorna o JSON
     header('Content-Type: application/json');
     echo json_encode($results);
     exit;
 }
 
+
+//-----GET streams de transmissão ao vivo
+//player_api.php?username=X&password=X&action=get_live_streams
 if (isset($_GET['action']) && in_array($_GET['action'], ['get_live_streams', 'get_vod_streams', 'get_series'])) {
     $action = $_GET['action'];
     $stream_type = '';
     $table = 'streams';
     $adulto = isset($result['adulto']) ? $result['adulto'] : 0;
 
+    // Define o tipo de stream e a tabela correspondente
     switch ($action) {
         case 'get_live_streams':
             $stream_type = 'live';
@@ -233,11 +263,12 @@ if (isset($_GET['action']) && in_array($_GET['action'], ['get_live_streams', 'ge
             break;
         case 'get_series':
             $stream_type = 'series';
-            $table = 'series'; 
+            $table = 'series'; // Usamos a tabela de séries.
             break;
     }
     $codigo_unico = gerarCodigoUnico();
 
+    // Prepara a query com ou sem filtro de adulto
     $query_str = "SELECT * FROM $table WHERE stream_type = :stream_type";
     if ($adulto == 0) {
         $query_str .= " AND is_adult = '0'";
@@ -247,17 +278,18 @@ if (isset($_GET['action']) && in_array($_GET['action'], ['get_live_streams', 'ge
     $query->bindValue(":stream_type", $stream_type);
     $query->execute();
 
+    // Processa os resultados
     $num = 0;
     $results = [];
     while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
         $num++;
-
+        // Monta os resultados com base no tipo de ação
         if ($action == 'get_live_streams') {
             $results[] = [
                 "num" => $num,
                 "name" => nl2br($row["name"]),
                 "stream_type" => $row["stream_type"],
-                "stream_id" => $row["id"],
+                "stream_id" => $row["id"],//.$codigo_unico,
                 "stream_icon" => $row["stream_icon"],
                 "epg_channel_id" => $row["epg_channel_id"],
                 "added" => $row["added"],
@@ -289,50 +321,56 @@ if (isset($_GET['action']) && in_array($_GET['action'], ['get_live_streams', 'ge
                 "custom_sid" => "",
                 "direct_source" => "",
             ];
-        }  
+        }  // Ajusta os campos para corresponderem ao formato do JSON desejado
         if ($action == 'get_series') {
-            $results[] = [
-                "num" => $num,
-                "name" => $row["name"] ?? "",
-                "title" => $row["name"] ?? "",
-                "year" => $row["year"] ?? "",
-                "stream_type" => $row["stream_type"],
-                "series_id" => $row["id"],
-                "cover" => $row["cover"] ?? "",
-                "plot" => $row["plot"] ?? "",
-                "cast" => !empty($row["cast"]) ? $row["cast"] : null,
-                "director" => !empty($row["director"]) ? $row["director"] : null,
-                "genre" => $row["genre"] ?? "",
-                "release_date" => $row["release_date"] ?? "",
-                "releaseDate" => $row["release_date"] ?? "",
-                "last_modified" => $row["last_modified"] ?? "",
-                "rating" => $row["rating"] ?? "0",
-                "rating_5based" => floatval($row["rating_5based"] ?? 0),
-                "backdrop_path" => !empty($row["backdrop_path"]) ? explode(",", $row["backdrop_path"]) : [],
-                "youtube_trailer" => !empty($row["youtube_trailer"]) ? $row["youtube_trailer"] : null,
-                "episode_run_time" => $row["episode_run_time"] ?? "0",
-                "category_id" => $row["category_id"],
-                "category_ids" => [$row["category_id"]],
-            ];         
+            $tmdb_id = "";
+                $results[] = [
+                    "num" => $num,
+                    "name" => $row["name"] ?? "",
+                    "title" => $row["name"] ?? "",
+                    "year" => $row["year"] ?? "",
+                    "stream_type" => $row["stream_type"],
+                    "series_id" => $row["id"],
+                    "cover" => $row["cover"] ?? "",
+                    "plot" => $row["plot"] ?? "",
+                    "cast" => !empty($row["cast"]) ? $row["cast"] : null,
+                    "director" => !empty($row["director"]) ? $row["director"] : null,
+                    "genre" => $row["genre"] ?? "",
+                    "release_date" => $row["release_date"] ?? "",
+                    "releaseDate" => $row["release_date"] ?? "",
+                    "last_modified" => $row["last_modified"] ?? "",
+                    "rating" => $row["rating"] ?? "0",
+                    "rating_5based" => floatval($row["rating_5based"] ?? 0),
+                    "backdrop_path" => !empty($row["backdrop_path"]) ? explode(",", $row["backdrop_path"]) : [],
+                    "youtube_trailer" => !empty($row["youtube_trailer"]) ? $row["youtube_trailer"] : null,
+                    "episode_run_time" => $row["episode_run_time"] ?? "0",
+                    "category_id" => $row["category_id"],
+                    "category_ids" => [$row["category_id"]],
+                ];            
         }
     }
 
+    // Filtro por category_id, se aplicável
     if (isset($category_id)) {
         $category_id_to_filter = $category_id;
         $results = array_filter($results, function ($item) use ($category_id_to_filter) {
             return $item['category_id'] == $category_id_to_filter;
         });
 
+        // Reorganiza os índices do array após a filtragem
         $results = array_values($results);
     }
 
+    // Retorna o JSON
     header('Content-Type: application/json');
     echo json_encode($results, JSON_PRETTY_PRINT);
     exit;
 }
 
+//-----GET de informações de filmes
+//player_api.php?username=X&password=X&action=get_vod_info&vod_id=X
 if (isset($_GET['action']) && isset($_GET['vod_id']) && $_GET['action'] === 'get_vod_info') {
-
+    
     $query = $conexao->prepare("SELECT * FROM streams WHERE id = :vod_id");
     $query->execute(array(":vod_id" => $vod_id));
     $info = array();
@@ -388,48 +426,60 @@ if (isset($_GET['action']) && isset($_GET['vod_id']) && $_GET['action'] === 'get
         "movie_data" => $movie_data
     );
 
+
     echo json_encode($result);
     exit;
 }
+//-----GET de informações de series
+//player_api.php?username=X&password=X&action=get_series_info&series_id=X
+if (isset($_GET['action'], $_GET['series_id']) && $_GET['action'] === 'get_series_info') {
+    // Obtém e valida o ID da série
+    $series_id = filter_input(INPUT_GET, 'series_id', FILTER_VALIDATE_INT);
 
-if (isset($_GET['action']) && isset($_GET['series_id']) && $_GET['action'] === 'get_series_info') {
+    if (!$series_id) {
+        http_response_code(400);
+        echo json_encode(["error" => "ID da série inválido."]);
+        exit;
+    }
 
-    $series_id = $_GET['series_id'];
-
+    // Busca informações da série
     $query = $conexao->prepare("SELECT * FROM series WHERE id = :series_id");
-    $query->execute(array(":series_id" => $series_id));
+    $query->execute([":series_id" => $series_id]);
     $row = $query->fetch(PDO::FETCH_ASSOC);
 
     if (!$row) {
         http_response_code(404);
-        echo json_encode(array("error" => "Série não encontrada."));
+        echo json_encode(["error" => "Série não encontrada."]);
         exit;
     }
-        $series = array(
-            "name" => $row["name"] ?? "",
-            "title" => $row["name"] ?? "",
-            "cover" => $row["cover"] ?? "",
-            "year" => $row["year"] ?? "",
-            "plot" => $row["plot"] ?? "",
-            "cast" => $row["cast"] ?? "",
-            "director" => $row["director"] ?? "",
-            "genre" => $row["genre"] ?? "",
-            "release_date" => $row["release_date"] ?? "",
-            "last_modified" => explode(",", $row["last_modified"] ?? ""),
-            "rating" => $row["rating"] ?? "",
-            "rating_5based" => $row["rating_5based"] ?? "",
-            "backdrop_path" => explode(",", $row["backdrop_path"] ?? ""),
-            "youtube_trailer" => $row["youtube_trailer"] ?? "",
-            "episode_run_time" => $row["episode_run_time"] ?? "",
-            "category_id" => $row["category_id"] ?? "",
-            "category_ids" => array($row["category_id"] ?? "")
-        );
 
+    $series = [
+        "name" => $row["name"] ?? "",
+        "title" => $row["name"] ?? "",
+        "cover" => $row["cover"] ?? "",
+        "year" => $row["year"] ?? "",
+        "plot" => $row["plot"] ?? "",
+        "cast" => $row["cast"] ?? "",
+        "director" => $row["director"] ?? "",
+        "genre" => $row["genre"] ?? "",
+        "release_date" => $row["release_date"] ?? "",
+        "last_modified" => !empty($row["last_modified"]) ? explode(",", $row["last_modified"]) : [],
+        "rating" => $row["rating"] ?? "",
+        "rating_5based" => $row["rating_5based"] ?? "",
+        "backdrop_path" => !empty($row["backdrop_path"]) ? explode(",", $row["backdrop_path"]) : [],
+        "youtube_trailer" => $row["youtube_trailer"] ?? "",
+        "episode_run_time" => $row["episode_run_time"] ?? "",
+        "category_id" => $row["category_id"] ?? "",
+        "category_ids" => [$row["category_id"] ?? ""]
+    ];
+
+    // Busca temporadas da série
     $query = $conexao->prepare("SELECT * FROM series_seasons WHERE series_id = :series_id");
-    $query->execute(array(":series_id" => $series_id));
-    $seasons = array();
+    $query->execute([":series_id" => $series_id]);
+    $seasons = [];
+
     while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-        $seasons[] = array(
+        $seasons[] = [
             "air_date" => $row["air_date"] ?? "",
             "episode_count" => $row["episode_count"] ?? "",
             "id" => $row["id"] ?? "",
@@ -438,26 +488,26 @@ if (isset($_GET['action']) && isset($_GET['series_id']) && $_GET['action'] === '
             "season_number" => $row["season_number"] ?? "",
             "cover" => $row["cover"] ?? "",
             "cover_big" => $row["cover_big"] ?? ""
-        );
+        ];
     }
 
+    // Busca episódios organizados por temporada
     $query = $conexao->prepare("SELECT * FROM series_episodes WHERE series_id = :series_id ORDER BY season, episode_num");
-    $query->execute(array(":series_id" => $series_id));
-    $episodes = array();
-    $season = null;
+    $query->execute([":series_id" => $series_id]);
+    $episodes = [];
 
     while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-        if ($row['season'] != $season) {
-            $episodes[$row['season']] = array();
-            $season = $row['season'];
+        $season = $row['season'] ?? 0;
+        if (!isset($episodes[$season])) {
+            $episodes[$season] = [];
         }
 
-        $episode = array(
+        $episodes[$season][] = [
             "id" => $row["id"] ?? "",
             "episode_num" => $row["episode_num"] ?? "",
             "title" => $row["title"] ?? "",
             "container_extension" => "mp4",
-            "info" => array(
+            "info" => [
                 "tmdb_id" => $row["tmdb_id"] ?? "",
                 "duration_secs" => $row["duration_secs"] ?? "",
                 "duration" => $row["duration"] ?? "",
@@ -465,28 +515,29 @@ if (isset($_GET['action']) && isset($_GET['series_id']) && $_GET['action'] === '
                 "cover_big" => $row["cover_big"] ?? "",
                 "movie_image" => $row["movie_image"] ?? "",
                 "plot" => $row["plot"] ?? ""
-            ),
-            "subtitles" => explode(",", $row["subtitles"] ?? ""),
+            ],
+            "subtitles" => !empty($row["subtitles"]) ? explode(",", $row["subtitles"]) : [],
             "custom_sid" => $row["custom_sid"] ?? "",
             "added" => $row["added"] ?? "",
-            "season" => $row["season"] ?? "",
+            "season" => $season,
             "direct_source" => ""
-        );
-        $episodes[$row['season']][] = $episode;
+        ];
     }
 
-    $result = array(
+    // Retorno final
+    header('Content-Type: application/json');
+    echo json_encode([
         "seasons" => $seasons,
         "info" => $series,
         "episodes" => $episodes
-    );
-
-    header('Content-Type: application/json');
-    echo json_encode($result);
+    ]);
     exit;
 }
 
-http_response_code(401); 
+
+
+
+http_response_code(401); // 401 Unauthorized
 $errorResponse = json_encode(["user_info" => ["auth" => 0]]);
 echo $errorResponse;
 exit();
